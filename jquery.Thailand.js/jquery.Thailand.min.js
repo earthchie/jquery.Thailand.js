cc07ce4ba14432147d7f78972a65c0b22e3bc38f
Thanarat Kuawattanaphan <earthchie@gmail.com>
2017-04-09 00:30:39 +0700

    rebase to z1.1.0

--- jquery.Thailand.js/jquery.Thailand.min.js
+++ jquery.Thailand.js/jquery.Thailand.min.js
@@ -1,6 +1,6 @@
 /**
  * @name jquery.Thailand.js
- * @version z1.0.2
+ * @version z1.1.0
  * @update Apr 8, 2017
  * @website https://github.com/earthchie/jquery.Thailand.js/tree/zipped_version
  * @license WTFPL v.2 - http://www.wtfpl.net/
