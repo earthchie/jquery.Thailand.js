6fbc2dba93c3d34b71a9ec3290292628a1ca7dfa
earthchie <earthchie@gmail.com>
2017-04-16 19:10:17 +0700

    to v1.3.0

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # jquery.Thailand.js
 ตัวช่วยกรอกที่อยู่ที่ดีที่สุดในไทย ไม่ต้องใช้ Server Side!
 
-อ่านแนวคิด และที่มาที่ไปได้ที่นี่ https://medium.com/@earthchie/ระบบ-auto-complete-ที่อยู่ไทย-อย่างที่มันควรเป็น-27360185d86a
+อ่านแนวคิด และที่มาที่ไปได้ที่นี่ [ระบบ Auto Complete ที่อยู่ไทย อย่างที่มันควรเป็น](https://medium.com/@earthchie/ระบบ-auto-complete-ที่อยู่ไทย-อย่างที่มันควรเป็น-27360185d86a)
 
-# Todo
-- Clean up repo
-- Need help! with database https://github.com/earthchie/jquery.Thailand.js/issues/4
+## Changelogs 1.3.0
+- อัพเดตจาก 1.2.0
+- เพิ่ม callback onDataFill() ตาม [request](https://github.com/earthchie/jquery.Thailand.js/issues/9)
+- แก้บัค ฟิลด์ข้อมูลไม่ยิง event change เมื่อเกิด autocomplete แล้ว (สำหรับตัวอย่างเพิ่มเติม ดูโค้ดใน [Demo](https://earthchie.github.io/jquery.Thailand.js/))
+- จัดระเบียบโครงสร้างโปรเจค และลบไฟล์ที่ไม่จำเป็นทิ้ง
+- เพิ่มฐานข้อมูลต้นฉบับ เป็นไฟล์ .xls อัพเดตข้อมูลล่าสุด เดือนตุลา 2559
+- ฟิลด์ใหม่ ``$search`` ใช้สำหรับค้นหา ดูตัวอย่างได้จากโค้ดใน [Demo](https://earthchie.github.io/jquery.Thailand.js/)
+
+## Todo
+- [x] Clean up repo
+- [ ] Need help! with database https://github.com/earthchie/jquery.Thailand.js/issues/4
 
 # Demo
-https://earthchie.github.io/jquery.Thailand.js/
+[https://earthchie.github.io/jquery.Thailand.js/](https://earthchie.github.io/jquery.Thailand.js/)
 
-# วิธีใช้
+## วิธีใช้
 
 1. ติดตั้ง Dependency ให้ครบ
 
@@ -23,8 +31,8 @@ https://earthchie.github.io/jquery.Thailand.js/
 2. ติดตั้ง jquery.Thailand.js
 
 ```html
-<link rel="stylesheet" href="jquery.Thailand.js/jquery.Thailand.min.css">
-<script type="text/javascript" src="jquery.Thailand.js/jquery.Thailand.min.js"></script>
+<link rel="stylesheet" href="./jquery.Thailand.js/dist/jquery.Thailand.min.css">
+<script type="text/javascript" src="./jquery.Thailand.js/dist/jquery.Thailand.min.js"></script>
 ```
 
 3. สร้าง input สำหรับกรอก ตำบล, อำเภอ, จังหวัด, รหัสไปรษณีย์
@@ -41,14 +49,21 @@ https://earthchie.github.io/jquery.Thailand.js/
 ```javascript
 
 $.Thailand({ 
-    database: './jquery.Thailand.js/db.zip', // path หรือ url ไปยัง zip
-    autocomplete_size: 10, // ถ้าไม่ระบุ ค่า default คือ 20
-    $district: $('#district'),
-    $amphoe: $('#amphoe'),
-    $province: $('#province'),
-    $zipcode: $('#zipcode'),
-    onComplete: function(){
-        console.log('Autocomplete is ready!')
+    
+    database: './jquery.Thailand.js/dist/db.json', // path หรือ url ไปยัง zip
+    autocomplete_size: 10, // ขนาดของตัวเลือก ถ้าไม่ระบุ ค่า default คือ 20
+
+    $district: $('[name="district"]'), // input ของตำบล
+    $amphoe: $('[name="amphoe"]'), // input ของอำเภอ
+    $province: $('[name="province"]'), // input ของจังหวัด
+    $zipcode: $('[name="zipcode"]'), // input ของรหัสไปรษณีย์
+
+    onDataFill: function(data){ // callback เมื่อเกิดการ auto complete ขึ้น
+        console.info('Data Filled', data);
+    },
+
+    onLoad: function(){ // callback เมื่อโหลดฐานข้อมูลเสร็จและระบบ Auto Complete พร้อมที่จะทำงาน
+        console.info('Autocomplete is ready!');
     }
 });
 
@@ -58,7 +73,7 @@ $.Thailand({
 
 ใน [v1.1.0](https://github.com/earthchie/jquery.Thailand.js/tree/fe302996ca72f156e1542048419399484431c391) เป็นต้นมามีการปรับเปลี่ยนโครงสร้างข้อมูล ภายใต้สมมุติฐานว่า Server รองรับ gzip รายละเอียดดังนี้
 
-| ไฟล์ | ขนาดเมื่อถูก Gzip |
+| ไฟล์ | ขนาดเมื่อถูก gzip |
 | --- | ---:|
 | JQL.min.js | 1.3 KB |
 | typeahead.bundle.js | 14.6 KB |
---
+++ jquery.Thailand.js/src/jquery.Thailand.js
@@ -0,0 +1,263 @@
+/**
+ * @name jquery.Thailand.js
+ * @version 1.3.0
+ * @update Apr 16, 2017
+ * @website https://github.com/earthchie/jquery.Thailand.js
+ * @license WTFPL v.2 - http://www.wtfpl.net/
+ *
+ * @dependency: jQuery <https://jquery.com/>
+ *              typeahead.js <https://twitter.github.io/typeahead.js/>
+ *              JQL.js <https://github.com/earthchie/JQL.js>
+ **/
+
+$.Thailand = function (options) {
+    'use strict';
+
+    options = $.extend({
+
+        database: './db.json',
+        autocomplete_size: 20,
+
+        onLoad: function () {},
+        onDataFill: function () {},
+
+        $district: false,
+        $amphoe: false,
+        $province: false,
+        $zipcode: false,
+        $search: false
+
+    }, options);
+
+    // get zip binary
+    $.getJSON(options.database, function (json) {
+        var DB = new JQL(preprocess(json)), // make json query-able
+            typehead_options = {
+                hint: true,
+                highlight: true,
+                minLength: 1
+            },
+            templates = { // template of autocomplete choices
+                empty: ' ',
+                suggestion: function (data) {
+                    return '<div>' + data.d + ' » ' + data.a + ' » ' + data.p + ' » ' + (data.z || '<i>ไม่มีรหัสไปรษณีย์</i>') + '</div>';
+                }
+            },
+            autocomplete_handler = function (e, v) { // set value when user selected autocomplete choice
+
+                if(options.$district){
+                    options.$district.val(v.d).trigger('change');
+                }
+                if(options.$amphoe){
+                    options.$amphoe.val(v.a).trigger('change');
+                }
+                if(options.$province){
+                    options.$province.val(v.p).trigger('change');
+                }
+                if(options.$zipcode){
+                    options.$zipcode.val(v.z).trigger('change');
+                }
+
+                if(typeof options.onDataFill === 'function'){
+                    options.onDataFill({
+                        district: v.d,
+                        amphoe: v.a,
+                        province: v.p,
+                        zipcode: v.z
+                    });
+                }
+                
+            };
+
+        // init auto complete for district
+        if(options.$district){
+            options.$district.typeahead(typehead_options, {
+                limit: options.autocomplete_size,
+                templates: templates,
+                source: function (str, callback) {
+                    var possibles = [];
+                    try {
+                        possibles = DB.select('*').where('d').match('^' + str).orderBy('d').fetch();
+                    } catch (e) {}
+                    callback(possibles);
+                },
+                display: function (data) {
+                    return data.d;
+                }
+            });
+        }
+
+        // init auto complete for amphoe
+        if(options.$amphoe){
+            options.$amphoe.typeahead(typehead_options, {
+                limit: options.autocomplete_size,
+                templates: templates,
+                source: function (str, callback) {
+                    var possibles = [];
+                    try {
+                        possibles = DB.select('*').where('a').match('^' + str).orderBy('a').fetch();
+                    } catch (e) {}
+                    callback(possibles);
+                },
+                display: function (data) {
+                    return data.a;
+                }
+            });
+        }
+
+        // init auto complete for province
+        if(options.$province){
+            options.$province.typeahead(typehead_options, {
+                limit: options.autocomplete_size,
+                templates: templates,
+                source: function(str, callback) {
+                    var possibles = [];
+                    try {
+                        possibles = DB.select('*').where('p').match('^' + str).orderBy('p').fetch();
+                    } catch (e) {}
+                    callback(possibles);
+                },
+                display: function (data) {
+                    return data.p;
+                }
+            });
+        }
+            
+
+        // init auto complete for zipcode
+        if(options.$zipcode){
+            options.$zipcode.typeahead(typehead_options, {
+                limit: options.autocomplete_size,
+                templates: templates,
+                source: function(str, callback) {
+                    var possibles = [];
+                    try {
+                        possibles = DB.select('*').where('z').match('^' + str).orderBy('z').fetch();
+                    } catch (e) {}
+                    callback(possibles);
+                },
+                display: function (data) {
+                    return data.z;
+                }
+            });
+        }
+
+        if(options.$search){
+            options.$search.typeahead(typehead_options, {
+                limit: options.autocomplete_size,
+                templates: templates,
+                source: function (str, callback) {
+                    var possibles = [], i;
+                    try {
+                        possibles = new JQL(possibles
+                                    .concat(DB.select('*').where('a').match('^' + str).fetch())
+                                    .concat(DB.select('*').where('d').match('^' + str).fetch())
+                                    .concat(DB.select('*').where('p').match('^' + str).fetch())
+                                    .concat(DB.select('*').where('z').match('^' + str).fetch())
+                                    .filter(function(self, index, parent){ // remove duplicated data
+                                        var isUnique = true
+                                        for(i = 0; i < parent.length; i++){
+                                            if(index !== i && parent[i].a == self.d && parent[i].d === self.d){
+                                                isUnique = false;
+                                            }
+                                        }
+                                        return isUnique;
+                                    })).select('*').orderBy('a').fetch();
+                    } catch (e) {}
+
+                    callback(possibles);
+                },
+                display: function (data) {
+                    return '';
+                }
+            });
+        }
+        
+        // on autocomplete
+        if(options.$district){
+            options.$district.bind('typeahead:select typeahead:autocomplete', autocomplete_handler);
+        }
+        if(options.$amphoe){
+            options.$amphoe.bind('typeahead:select typeahead:autocomplete', autocomplete_handler);
+        }
+        if(options.$province){
+            options.$province.bind('typeahead:select typeahead:autocomplete', autocomplete_handler);
+        }
+        if(options.$zipcode){
+            options.$zipcode.bind('typeahead:select typeahead:autocomplete', autocomplete_handler);
+        }
+        if(options.$search){
+            options.$search.bind('typeahead:select typeahead:autocomplete', autocomplete_handler);
+        }
+
+        // callback
+        if (typeof options.onLoad === 'function') {
+            options.onLoad();
+        }
+
+        // callback, fallback version 1.2.
+        if (typeof options.onComplete === 'function') {
+            options.onComplete();
+        }
+
+    })
+    .fail(function (err) {
+        throw new Error('File "' + options.database + '" is not exists.');
+    });
+
+    function preprocess(data) {
+        var lookup = []
+        var words = []
+        var useLookup = false
+        if (data.lookup && data.words) {
+            // compact with dictionary and lookup
+            useLookup = true
+            lookup = data.lookup.split('|')
+            words = data.words.split('|')
+            data = data.data
+        }
+
+        function t(text) {
+            function repl(m) {
+                var ch = m.charCodeAt(0)
+                return words[ch < 97 ? ch - 65 : 26 + ch - 97]
+            }
+            if (!useLookup) {
+                return text
+            }
+            if (typeof text === 'number') {
+                text = lookup[text]
+            }
+            return text.replace(/[A-Z]/ig, repl)
+        }
+
+        if (!data[0].length) {
+            // non-compacted database
+            return data;
+        }
+        // compacted database in hierarchical form of:
+        // [["province",[["amphur",[["district",["zip"...]]...]]...]]...]
+        var expanded = [];
+        data.forEach(function (provinceEntry) {
+            var province = provinceEntry[0];
+            var amphurList = provinceEntry[1];
+            amphurList.forEach(function (amphurEntry) {
+                var amphur = amphurEntry[0];
+                var districtList = amphurEntry[1];
+                districtList.forEach(function (districtEntry) {
+                    var district = districtEntry[0];
+                    var zipCodeList = districtEntry[1] instanceof Array ? districtEntry[1] : [districtEntry[1]];
+                    zipCodeList.forEach(function (zipCode) {
+                        expanded.push({
+                            d: t(district),
+                            a: t(amphur),
+                            p: t(province),
+                            z: zipCode
+                        });
+                    });
+                });
+            });
+        });
+        return expanded;
+    }
+};
