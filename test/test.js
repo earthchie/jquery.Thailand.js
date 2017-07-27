3ce80e2f66b28c6aef2e0527f7f84d31547f32b0
Nati Namvong <nati@flyingcomma.com>
2017-07-27 15:05:26 +0700

    Add Standard style(ESLint)

--- test/test.js
+++ test/test.js
@@ -1,25 +1,28 @@
 'use strict'
 
+var mocha = require('mocha')
+var describe = mocha.describe
+var it = mocha.it
 var expect = require('chai').expect
 var db = require('../src/thai-address-database')
 
-describe('#find', function() {
-  it('findAddressByDistrict', function() {
+describe('#find', function () {
+  it('findAddressByDistrict', function () {
     var result = db.findAddressByDistrict('อรัญประเทศ')
     expect(result.length).to.equal(1)
   })
 
-  it('findAddressByAmphoe', function() {
+  it('findAddressByAmphoe', function () {
     var result = db.findAddressByAmphoe('อรัญประเทศ')
     expect(result.length).to.equal(13)
   })
 
-  it('findAddressByProvince', function() {
+  it('findAddressByProvince', function () {
     var result = db.findAddressByProvince('อรัญประเทศ')
     expect(result.length).to.equal(0)
   })
 
-  it('findAddressByProvince', function() {
+  it('findAddressByProvince', function () {
     var result = db.findAddressByProvince('สระแก้ว')
     expect(result.length).to.equal(20)
   })
